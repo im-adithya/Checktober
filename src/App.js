564b7010dc44b0f6dd21318ed5c226779382ed50
Adithya Vardhan <imadithyavardhan@gmail.com>
2022-01-07 21:54:01 +0530

    add: g-tag

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from "react";
 import { Image } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -14,6 +15,25 @@ import ProgressScreen from "./screens/ProgressScreen";
 
 import githublogo from "./assets/github-logo.svg";
 import branchdesign from "./assets/branch-design.svg";
+
+import firebase from "firebase/app";
+import "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+function initFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+}
+
+initFirebase();
+firebase.analytics();
 
 function App() {
   return (
